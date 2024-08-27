c4e7fa95f1621af7937249ab79c45974eee2b1ce
Lee-Jinyong <yjy1480@gmail.com>
2024-08-27 23:24:01 +0900

    modify: Prettier 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
 import pluginReactRefresh from 'eslint-plugin-react-refresh';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -35,6 +37,8 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+  prettierRecommended,
   {
     rules: {
       ...pluginReactHooks.configs.recommended.rules,
