2858c7012ddca08e9fd71fd5dd57484e998cb6e7
Lee-Jinyong <yjy1480@gmail.com>
2024-08-27 13:40:56 +0900

    modify: eslint.config.js 수정
    - 지정한 파일 확장자에 대해서 오류 검사 시행하는 스크립트 명령어 추가

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,37 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
