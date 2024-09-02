84d09485627423ed8c6fa69e95cde263dcb99797
Lee-Jinyong <yjy1480@gmail.com>
2024-09-02 13:52:06 +0900

    modify: 페이지 별 헤더 반응성 구현
    - 콘텐츠 페이지인지, 로그인 관련 페이지인지 체크
    - 로그인 상태인지, 게스트 상태인지 체크

--- src/components/AppHeader/components/AppNav/index.tsx
+++ src/components/AppHeader/components/AppNav/index.tsx
@@ -9,13 +9,15 @@ import BoxButton from '../BoxButton';
 
 function GlobalNav() {
   const [navigationList] = useState<ExtendedRouteObject[]>(navigationItems);
-  const [isContentPage, setIsContentPage] = useState(true);
+  // 콘텐츠 페이지인지 확인하는 상태(로그인 관련 페이지가 아닌지 확인)
+  const [isContentPage, setIsContentPage] = useState(false);
+  // 로그인 했는지 확인하는 상태
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
     //로그인, 회원가입 페이지인지 체크 후 헤더 형식 변경을 위한 코드
     //로그인 validation 구현 후 수정해야 함
-    if (true) {
+    if (isContentPage) {
       setIsContentPage(true);
     } else {
       setIsContentPage(false);
@@ -25,9 +27,17 @@ function GlobalNav() {
   return (
     <nav>
       <ul className={S.component}>
-        <li className={S.HomeButton}>
-          {isContentPage ? <HomeButton imgSrc={'/Icon/Logo.svg'} /> : <HomeButton imgSrc={'/JJcom.svg'} />}
-        </li>
+        {isContentPage ? (
+          // 콘텐츠 페이지일 때
+          <li className={S.HomeButtonContent}>
+            <HomeButton imgSrc={'/Icon/Logo.svg'} />
+          </li>
+        ) : (
+          // 콘텐츠 페이지가 아닐 때(로그인 관련 페이지)
+          <li className={S.HomeButtonLogin}>
+            <HomeButton imgSrc={'/Icon/Logo.svg'} />
+          </li>
+        )}
         <li className={S.menuButtonWrapper}>
           <ul>
             {navigationList.map(({ path, text }) =>
@@ -47,14 +57,32 @@ function GlobalNav() {
             )}
           </ul>
         </li>
-        <li className={S.formWrapper}>
-          <ul>
-            <li>
-              <SearchBox />
-            </li>
-            <li>{isAuth ? <BoxButton text={'마이페이지'} /> : <BoxButton text={'로그인'} />}</li>
-          </ul>
-        </li>
+        {/* 콘텐츠 페이지이고, 로그인 상태일 때 */}
+        {isContentPage && isAuth ? (
+          <li className={S.formWrapper}>
+            <ul>
+              <li>
+                <SearchBox />
+              </li>
+              <li>
+                <BoxButton text={'마이페이지'} />
+              </li>
+            </ul>
+          </li>
+        ) : null}
+        {/* 콘텐츠 페이지이고, 게스트 상태일 때 */}
+        {isContentPage && !isAuth ? (
+          <li className={S.formWrapper}>
+            <ul>
+              <li>
+                <SearchBox />
+              </li>
+              <li>
+                <BoxButton text={'로그인'} />
+              </li>
+            </ul>
+          </li>
+        ) : null}
       </ul>
     </nav>
   );
