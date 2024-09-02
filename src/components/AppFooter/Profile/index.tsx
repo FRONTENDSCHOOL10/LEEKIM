6271a443d865758369f465e3bc270bf0a7f7ddad
Lee-Jinyong <yjy1480@gmail.com>
2024-09-02 13:39:53 +0900

    modify: 외부 링크 NavLink태그 a태그로 변경

--- src/components/AppFooter/Profile/index.tsx
+++ src/components/AppFooter/Profile/index.tsx
@@ -1,13 +1,14 @@
 import { memo } from 'react';
 import S from './style.module.scss';
-import { NavLink } from 'react-router-dom';
 
 function Profile({ name, email, link }: { name: string; email: string; link: string }) {
   return (
     <div className={S.component}>
       <p>{name}</p>
       <p>{email}</p>
-      <NavLink to={link}>GITHUB</NavLink>
+      <a href={link} target="_blank" rel="noreferrer noopener">
+        GITHUB
+      </a>
     </div>
   );
 }
