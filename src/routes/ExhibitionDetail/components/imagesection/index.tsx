4898709ac9e3db6304303c4fa74c7bbaf13a2e96
chlyun <rkdmf1306@gmail.com>
2024-09-03 10:25:19 +0900

    feat: 포스터 컴포넌트

---
+++ src/routes/ExhibitionDetail/components/imagesection/index.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from 'react';
+import S from './style.module.scss';
+import web from '/icon/web.svg';
+
+interface PosterProps {
+  url: string;
+  image: string;
+}
+
+function Poster({ url, image }: PosterProps): ReactElement {
+  return (
+    <div className={S.poster}>
+      <a href={url} className={S.container} target="_blank" rel="noopener noreferrer">
+        <img src={image} alt="Exhibition Poster" className={S.posterImage} />
+      </a>
+      <div className={S.info}>
+        <img src={web} alt="" />
+        <p>포스터를 누르면 웹사이트로 이동해요!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Poster;
