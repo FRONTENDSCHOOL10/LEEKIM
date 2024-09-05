561563ab8866cf003b4324d7cf3cb1e0cdb50a06
chlyun <rkdmf1306@gmail.com>
2024-09-05 10:12:08 +0900

    Modify: tag NavLink설정

--- src/routes/ExhibitionDetail/components/tag/index.tsx
+++ src/routes/ExhibitionDetail/components/tag/index.tsx
@@ -1,12 +1,17 @@
 import { ReactElement } from 'react';
 import S from './style.module.scss';
+import { NavLink } from 'react-router-dom';
 
 interface TagProps {
   text: string;
 }
 
 function Tag({ text }: TagProps): ReactElement {
-  return <span className={S.tag}>#{text}</span>;
+  return (
+    <NavLink to={'/'}>
+      <span className={S.tag}>#{text}</span>
+    </NavLink>
+  );
 }
 
 export default Tag;
