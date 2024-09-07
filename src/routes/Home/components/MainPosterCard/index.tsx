a5a7dfb34e99ee283a252cf839852068175c0130
Lee-Jinyong <yjy1480@gmail.com>
2024-09-07 17:25:25 +0900

    feature: 메인페이지 일부 완성
    - 메인 슬로건 구현
    - 메인 포스터 카드 구현
    - 전시 등록 바로가기 버튼 구현
    - 추천 배너 구현

---
+++ src/routes/Home/components/MainPosterCard/index.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react';
+import S from './style.module.scss';
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import axios from 'axios';
+import getImageURL from '@/utils/getImageURL';
+
+const button = {
+  rest: { opacity: 1, scale: 1 },
+  hover: { opacity: 1, scale: 1.1, y: -50 },
+  pressed: { opacity: 1, scale: 0.95 },
+};
+
+const dbUrl = import.meta.env.VITE_DB_URL;
+
+function MainPosterCard() {
+  // ExhibitionData 타입 삽입해야함
+  const [posterCardData, setPosterCardData] = useState<ExhibitionData | null>(null);
+
+  useEffect(() => {
+    const fetchExhibitionData = async () => {
+      try {
+        const response = await axios.get(
+          `${dbUrl}/api/collections/Exhibition/records?filter=(IsApprove=true%26%26IsHorizontal=false)&sort=@random&page=1&perPage=5`
+        );
+        setPosterCardData(response.data.items);
+      } catch (err) {
+        throw new Error('데이터를 불러오는 데 실패했습니다.');
+      }
+    };
+
+    fetchExhibitionData();
+  }, []);
+
+  if (!posterCardData) return <div>로딩 중</div>;
+
+  return (
+    <>
+      <section className={S.component}>
+        <ul className={S.posterCardWrapper}>
+          {posterCardData.map((item, index) =>
+            item ? (
+              <li key={index} className={S.posterCardList}>
+                <NavLink to={`/exhibition/Detail/${item.id}`}>
+                  <motion.div variants={button} initial="rest" whileHover="hover" whileTap="pressed">
+                    <img src={getImageURL(item)} alt="" className={S.posterCard} />
+                  </motion.div>
+                </NavLink>
+              </li>
+            ) : null
+          )}
+        </ul>
+        <div className={S.box}>
+          <span>우리학교 졸업 전시를</span>
+          <br />
+          <span>지금</span>
+          <img src="/Icon/TextLogo.svg" alt="" />
+          <span>에 등록해 보세요!</span>
+          <NavLink to={'/registerExhi'}>전시 등록 바로가기</NavLink>
+          <br />
+          <span>모든 졸업 전시는 관리자의 승인을 거친 후 등록됩니다.</span>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default MainPosterCard;
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,12 +1,21 @@
 import useDocumentTitle from '@/hooks/useDocumentTitle';
 import S from './style.module.scss';
+import Slogan from './components/Slogan/';
+import MainPosterCard from './components/MainPosterCard';
+import RecommendBanner from './components/RecommendBanner';
 
 function HomePage() {
   useDocumentTitle('미래를 엿보는 창, 졸전.com | JJ.com');
 
   return (
     <main id="page" className={S.component}>
-      홈 페이지
+      <h1 className="sr-only">졸전닷컴 메인페이지</h1>
+      <Slogan />
+      <MainPosterCard />
+      {/* 진행 중 전시 컴포넌트 */}
+      {/* 진행 예정 전시 컴포넌트 */}
+      <RecommendBanner />
+      {/* 최근 본 전시 */}
     </main>
   );
 }
