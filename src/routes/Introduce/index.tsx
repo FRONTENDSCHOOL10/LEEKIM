40859fd375feab688708db979a144f4964c0f56a
chlyun <rkdmf1306@gmail.com>
2024-09-09 10:32:01 +0900

    Feat: 졸업 전시 등록 방법 컴포넌트 추가

--- src/routes/Introduce/index.tsx
+++ src/routes/Introduce/index.tsx
@@ -1,12 +1,19 @@
 import useDocumentTitle from '@/hooks/useDocumentTitle';
 import S from './style.module.scss';
+import Profile from './components/team';
+import Description from './components/description';
+import Link from './components/link';
+import MainTitle from './components/mainTitle';
 
 export function Component() {
   useDocumentTitle('사이트 소개 | JJ.com');
 
   return (
-    <main id="page" className={S.component}>
-      사이트 소개 페이지
+    <main className={S.component}>
+      <MainTitle />
+      <Link />
+      <Description />
+      <Profile />
     </main>
   );
 }
