157ec76c573f4acdb3ed8f3d748d1b8cf2555c4c
chlyun <rkdmf1306@gmail.com>
2024-09-11 16:30:16 +0900

    modify:  전시 날짜 스타일변경완료

--- src/routes/RegisterExhibition/components/requiredinfo/index.tsx
+++ src/routes/RegisterExhibition/components/requiredinfo/index.tsx
@@ -7,38 +7,38 @@ function RequireInfo(): ReactElement {
       <div className={S.formrow}>
         <div className={S.formgroup}>
           <label htmlFor="School">학교명/학부명</label>
-          <input type="text" id="School" name="School" placeholder="OO대학교" required />
+          <input type="text" id="School" name="School" placeholder="OO대학교" className={S.input} required />
         </div>
         <div className={S.formgroup}>
           <label htmlFor="Major">학과</label>
-          <input type="text" id="Major" name="Major" placeholder="OO과" required />
+          <input type="text" id="Major" name="Major" placeholder="OO과" className={S.input} required />
         </div>
       </div>
       <div className={S.formgroup}>
         <label htmlFor="Title">전시 이름</label>
-        <input type="text" id="Title" name="Title" placeholder="전시회 타이틀" required />
+        <input type="text" id="Title" name="Title" placeholder="전시회 타이틀" className={S.input} required />
       </div>
       <div className={S.formgroup}>
         <label htmlFor="Address">전시 장소</label>
-        <input id="Address" name="Address" required />
+        <input id="Address" name="Address" className={S.input} required />
       </div>
       <div className={S.formrow}>
         <div className={S.formgroup}>
           <label htmlFor="Start">전시 시작일</label>
-          <input type="date" id="Start" name="Start" required />
+          <input type="date" id="Start" name="Start" placeholder="날짜를 선택해주세요" className={S.date} required />
         </div>
         <div className={S.formgroup}>
           <label htmlFor="End">전시 종료일</label>
-          <input type="date" id="End" name="End" required />
+          <input type="date" id="End" name="End" placeholder="날짜를 선택해주세요" className={S.date} required />
         </div>
       </div>
       <div className={S.formgroup}>
         <label htmlFor="Time">전시 시간</label>
-        <input type="text" id="Time" name="Time" placeholder="Ex) 09:00 ~ 18:00" required />
+        <input type="text" id="Time" name="Time" className={S.input} placeholder="Ex) 09:00 ~ 18:00" required />
       </div>
       <div className={S.formgroup}>
         <label htmlFor="Introduce">전시 설명</label>
-        <textarea id="Introduce" name="Introduce" required />
+        <textarea id="Introduce" name="Introduce" className={S.input} required />
       </div>
     </div>
   );
