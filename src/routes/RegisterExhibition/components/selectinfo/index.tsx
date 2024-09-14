f6c3883467a871f6c65bdb0e5f5eb41389703e84
chlyun <rkdmf1306@gmail.com>
2024-09-15 04:58:50 +0900

    feat: 전시 등록페이지 컴포넌트

--- src/routes/RegisterExhibition/components/selectinfo/index.tsx
+++ src/routes/RegisterExhibition/components/selectinfo/index.tsx
@@ -1,18 +1,44 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import S from './style.module.scss';
 
-function SelectInfo(): ReactElement {
+interface SelectInfoProps {
+  exhibitionData: {
+    URL: string;
+    Contact: string;
+  };
+  onInputChange: (name: string, value: string) => void;
+}
+
+function SelectInfo({ exhibitionData, onInputChange }: SelectInfoProps): ReactElement {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onInputChange(e.target.name, e.target.value);
+  };
+
   return (
     <div className={S.selectinfo}>
       <h3>선택 정보 등록</h3>
       <div className={S.formrow}>
         <div className={S.formgroup}>
           <label htmlFor="URL">온라인 전시회 주소/SNS 주소</label>
-          <input type="text" id="URL" name="URL" placeholder="Ex) https://www.exhibitionSite.com" />
+          <input
+            type="text"
+            id="URL"
+            name="URL"
+            value={exhibitionData.URL}
+            onChange={handleChange}
+            placeholder="Ex) https://www.exhibitionSite.com"
+          />
         </div>
         <div className={S.formgroup}>
           <label htmlFor="Contact">부가정보</label>
-          <input type="text" id="Contact" name="Contact" placeholder="연락처, 전시회 정보 문의방법" />
+          <input
+            type="text"
+            id="Contact"
+            name="Contact"
+            value={exhibitionData.Contact}
+            onChange={handleChange}
+            placeholder="연락처, 전시회 정보 문의방법"
+          />
         </div>
       </div>
     </div>
