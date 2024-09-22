03872271c2f4bc6d22eb02a427a1786862c1454a
Lee-Jinyong <yjy1480@gmail.com>
2024-09-22 19:46:22 +0900

    modify: 상세페이지 태그 링크 삭제

--- src/routes/ExhibitionDetail/components/tag/index.tsx
+++ src/routes/ExhibitionDetail/components/tag/index.tsx
@@ -1,17 +1,12 @@
 import { ReactElement } from 'react';
 import S from './style.module.scss';
-import { NavLink } from 'react-router-dom';
 
 interface TagProps {
   text: string;
 }
 
 function Tag({ text }: TagProps): ReactElement {
-  return (
-    <NavLink to={'/'}>
-      <span className={S.tag}>#{text}</span>
-    </NavLink>
-  );
+  return <span className={S.tag}>#{text}</span>;
 }
 
 export default Tag;
